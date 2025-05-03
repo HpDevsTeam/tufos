0e56a78d0e73cf54f6bb178cf60ddc83fc7b0a8f
HpDevsTeam <teamhpinfinit@gmail.com>
2025-05-03 01:36:07 -0300

    up

---
+++ quadrinhos/js.js
@@ -0,0 +1,12 @@
+const menu = () => {
+    const oi = document.getElementById("menus");
+    const jk = document.getElementById("close");
+    if(oi.style.display == "none" || oi.style.display == ""){
+         oi.style.display = "block";
+         jk.src = "../img/icons/close-menu.png";
+    }
+    else{
+        oi.style.display = "none";
+        jk.src = "../img/icons/menu.png";
+    }
+}
